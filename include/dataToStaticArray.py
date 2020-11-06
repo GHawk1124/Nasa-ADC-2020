d5d4a27afad3d6bf591911916dd1cfad56c391cf
Fred Hancock <yxprogramming@protonmail.com>
2020-11-05 21:02:37 -0700

    Broken pathfinding, I'll fix it later

--- include/dataToStaticArray.py
+++ include/dataToStaticArray.py
@@ -1,12 +1,14 @@
-with open("../assets/data/fy20_adc_data_file_88_degrees.csv") as datafile:
+total_lines = 0
+with open("../assets/data/fy20_adc_data_file_high_precision.csv") as datafile:
 	final = []
 	for line in datafile.readlines():
 		intermediary = []
 		for i in line.strip().split(','):
 			intermediary.append(float(i))
 		final.append(intermediary)
+		total_lines += 1
 
-string = "#pragma once\n\nconst float[7223208][4] originalData = {\n"
+string = "#pragma once\n\nconst float originalData[" + str(total_lines) + "][4] = {\n"
 for datapoint in final:
 	string += "\t{" + "{}, {}, {}, {}".format(datapoint[0], datapoint[1], datapoint[2], datapoint[3]) + "},\n"
 string += "};"
