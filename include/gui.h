6a34b9810c413d50377d6d98595445b7144f3e88
LtPeriwinkle <not.yet.periwinkle@gmail.com>
2020-11-09 21:10:12 -0700

    add basic gui function

--- include/gui.h
+++ include/gui.h
@@ -5,20 +5,52 @@
 #include "imgui_impl_vulkan.h"
 
 inline void showGui() {
-    static float f = 0.0f;
-    static int counter = 0;
+    //create a window called "Settings" and add stuff to it
+    ImGui::Begin("Settings");
+    static int slope = 15;
+    static int distance = 0;
 
-    ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
+    //create a collapsing dropdown inside the window
+    if (ImGui::CollapsingHeader("Pathfinding Controls")) {
+        //sliders to control pathfinding algorithm params
+        ImGui::SliderInt("Max slope angle", &slope, 5, 40, "%f°");
+        ImGui::SliderInt("Max checkpoint distance", &distance, 0, 50, "%fm");
+        ImGui::Button("Recompute path");
+        ImGui::SameLine();
+        //create a hoverable tooltip (on same line)
+        ImGui::TextDisabled("(?)");
+        if (ImGui::IsItemHovered()) {
+            ImGui::BeginTooltip();
+            ImGui::PushTextWrapPos(ImGui::GetFontSize() * 35.0f);
+            ImGui::TextUnformatted("This may take a long time");
+            ImGui::PopTextWrapPos();
+            ImGui::EndTooltip();
+        }
+    }
 
-    ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
+    //collapsing dropdown #2
+    if (ImGui::CollapsingHeader("Recolor options")) {
+        const char* items[] = {"None", "Altitude", "Slope"};
+        static int current_index = 0;
+        const char* combo_label = items[current_index];
 
-    ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
+        //show dropdown select menu, keep the selected item selected.
+        if (ImGui::BeginCombo("Color type", combo_label, 0)) {
+            for (int n = 0; n < IM_ARRAYSIZE(items); n++) {
+                const bool is_selected = (current_index == n);
 
-    if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
-      counter++;
-    ImGui::SameLine();
-    ImGui::Text("counter = %d", counter);
+                if (ImGui::Selectable(items[n], is_selected)) {
+                    current_index = n;
+                }
 
+                if (is_selected) {
+                    ImGui::SetItemDefaultFocus();
+                }
+            }
+
+            ImGui::EndCombo();
+        }
+    }
     ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
     ImGui::End();
 }
